7c188e0fb0f9d589afe0be17bb8ee6cee9378ec5
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2020-11-18 09:08:04 +0000

    final changes

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 var readlineSync = require("readline-sync");
+const chalk = require('chalk');
 function check(year){
-  if(year%4==0){
-    console.log("Wow! You were born in a leap year.");
-    console.log("Take a screenshot & Share it with your  friends.");
+  if(year%400==0){
+    console.log(chalk.black.bgGreen("Wow! You were born in a leap year."));
+    console.log(chalk.blue.bgYellow.bold("Take a screenshot & Share it with your  friends."));
+  }
+  else if(year%100==0){
+    console.log(chalk.bgRed("You were not born in a leap year."));
+  }
+  else if(year%4==0){
+    console.log(chalk.black.bgGreen("Wow! You were born in a leap year."));
+    console.log(chalk.blue.bgYellow.bold("Take a screenshot & Share it with your  friends."));
   }
   else{
-    console.log("You were not born in a leap year.");
+    console.log(chalk.bgRed("You were not born in a leap year."));
   }
 }
 
 function getInput(){
-  let userInput = readlineSync.question("Enter your birthdate (mm/dd/yyyy): ");
+  let userInput = readlineSync.question(chalk.green("Enter your birthdate (mm/dd/yyyy): "));
   let birthdate = new Date(userInput);
   if(birthdate=="Invalid Date"){
-    console.log("You have entered wrong input. ");
+    console.log(chalk.red("You have entered wrong input. "));
     getInput();
   }
   return birthdate.getYear();
